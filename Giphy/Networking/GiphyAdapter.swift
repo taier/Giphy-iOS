1630a679d26c1d571a43328680d916374b49cabc
Deniss Kaibagarovs <deniss.kaibagarovs@gmail.com>
2018-01-03 08:05:12 +0200

    Improve downloading and parsing handler

--- Giphy/Networking/GiphyAdapter.swift
+++ Giphy/Networking/GiphyAdapter.swift
@@ -11,63 +11,65 @@ import UIKit
 class GiphyAdapter: NSObject, NetworkingCallsProtocol {
     
     let API_KEY = "kpPeUe6TpJCHlCrgUrR68M9lEaISJPEP"
-    let IMG_DOWNLOAD_PER_REQUEST_COUNT = "100"
     
     func getImagesURLsForQuery(query: String, limit: Int, offset: Int, completion: @escaping (Array<AnyObject>) -> Void) {
         
         var returnImages = Array<AnyObject> ()
-        
-        // Excape input
-        let excapedInput = query.addingPercentEncoding(
-            withAllowedCharacters: CharacterSet.urlQueryAllowed)
-        
-        // Prepare request URL
-        let requestUrl = "https://api.giphy.com/v1/gifs/search?q=" + excapedInput!
-        
-        let urlWithParams = requestUrl + "&api_key=kpPeUe6TpJCHlCrgUrR68M9lEaISJPEP&limit=" + String(limit) + "&offset=" + String(offset)
-        
-        // Create NSURL Ibject
-        let myUrl = URL(string: urlWithParams);
-        
-        // Creaste URL Request
-        var request = URLRequest(url:myUrl!)
-        
-        // Set request HTTP method to GET.
+        let myUrl = self.composeRequestURL(query: query, limit: limit, offset: offset);
+        var request = URLRequest(url:myUrl)
         request.httpMethod = "GET"
         
-        // Excute HTTP Request
         let task = URLSession.shared.dataTask(with: request) {
             data, response, error in
         
-            // Check for error
-            if error != nil
-            {
+            if error != nil {
                 print("error=\(error as Optional)")
                 return
             }
-            do {
-                // Obtain gif urls from JSON
-                if let json = try JSONSerialization.jsonObject(with: data!, options:.allowFragments) as? [String:Any] {
-                    if let data = json["data"] as? [AnyObject] {
-                        for item in data  {
-                            if let images = item["images"] as? [String:Any] {
-                                if let fixed_height_downsampled = images["fixed_height_downsampled"] as? [String:AnyObject] {
-                                    if let url = fixed_height_downsampled["url"] {
-                                        returnImages.append(url);
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            } catch let err{
-                print(err.localizedDescription)
-            }
+            
+            returnImages = self.parse(data:data!)
             
             DispatchQueue.main.async {
                 completion(returnImages)
             }
         }
+        
         task.resume()
     }
+    
+    func composeRequestURL(query: String, limit: Int, offset: Int) -> URL {
+        // Excape input
+        let excapedInput = query.addingPercentEncoding(
+            withAllowedCharacters: CharacterSet.urlQueryAllowed)
+        
+        let requestUrl = "https://api.giphy.com/v1/gifs/search?q=" + excapedInput!
+        let urlWithParams = requestUrl + "&api_key=" + API_KEY + "&limit=" + String(limit) + "&offset=" + String(offset)
+        let myUrl = URL(string: urlWithParams);
+        
+        return myUrl!;
+    }
+    
+    func parse(data: Data) -> Array<AnyObject> {
+        var returnImages = Array<AnyObject> ()
+        do {
+            if let json = try JSONSerialization.jsonObject(with:data, options:.allowFragments) as? [String:Any] {
+                if let data = json["data"] as? [AnyObject] {
+                    for item in data  {
+                        if let images = item["images"] as? [String:Any] {
+                            if let fixed_height_downsampled = images["fixed_height_downsampled"] as? [String:AnyObject] {
+                                if let url = fixed_height_downsampled["url"] {
+                                    returnImages.append(url);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        } catch let err{
+            print(err.localizedDescription)
+        }
+    
+        return returnImages;
+    }
+
 }
